4cea33bb779b46744e09bec1b9948b8d778ea880
nathbout2 <nathbout2@gmail.com>
2021-06-24 12:00:00 +0200

    nav-bar-boots

--- page.php
+++ page.php
@@ -1,18 +1,19 @@
 <?php include 'header.php'; ?>
 <p>Cette page est page.php </p>
-
-
+<div class="container">
 <?php if (have_posts()):while(have_posts()):the_post();?>
 
 
 <h1><?php the_title();?></h1>
 
-<?php the_post_thumbnail();?>
 
-<p>publié par <?php the_author();?>,le <?php the_date();?></p>
-<p><?php the_excerpt();?></p>
+<p><?php the_content();?></p>
 
 <?php endwhile; endif; ?>
+
+</div>
+
+
 
 
 <?php include 'footer.php'; ?>
